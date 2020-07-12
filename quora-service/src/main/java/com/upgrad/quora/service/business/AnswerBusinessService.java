cb68139a9b6ff1f206e499cc99392d2600f3220f
Pritam Sen <pritamsen@gmail.com>
2020-07-12 12:10:29 +0200

    added functionality to getAnswerByID, verifyAnswerBelongsToUser, updateAnswer

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
@@ -3,12 +3,15 @@ package com.upgrad.quora.service.business;
 import com.upgrad.quora.service.dao.AnswerDao;
 import com.upgrad.quora.service.entity.AnswerEntity;
 import com.upgrad.quora.service.entity.UserAuthenticationEntity;
+import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.NoResultException;
 
 @Service
 public class AnswerBusinessService {
@@ -23,5 +26,40 @@ public class AnswerBusinessService {
             throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to post an answer");
         }
         return answerDao.createAnswer(answerEntity);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity getAnswerById(String uuid) throws AnswerNotFoundException {
+        AnswerEntity answerEntity = answerDao.getAnswerById(uuid);
+        if (answerEntity == null)
+        {
+            throw new AnswerNotFoundException("ANS-001","Entered answer uuid does not exist");
+        }
+        return answerEntity;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity verifyAnswerBelongsToUser(UserAuthenticationEntity userAuthEntity, AnswerEntity answerEntity) throws AuthorizationFailedException {
+
+        UserEntity userEntity = userAuthEntity.getUser();
+
+        if(userAuthEntity.getLogoutAt()!=null){
+            throw new AuthorizationFailedException("ATHR-002" ,"User is signed out.Sign in first to edit an answer");
+        }
+        String auuid = answerEntity.getUuid();
+        String uuuid = userEntity.getUuid();
+        AnswerEntity checkedAnswer = answerDao.verifyAnswerBelongsToUser(auuid,uuuid);
+        if(checkedAnswer==null)
+        {
+            throw new AuthorizationFailedException("ATHR-003","Only the answer owner can edit or delete the answer");
+        }
+        return checkedAnswer;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity updateAnswer(AnswerEntity answerEntity)
+    {
+        AnswerEntity updateAnswer = answerDao.updateAnswer(answerEntity);
+        return updateAnswer;
     }
 }
