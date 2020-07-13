f93c7a0b22fe0aba7211a0579f31b78b999a01e3
ravi.athmakuri <ravi.athmakuri93@gmail.com>
2020-07-13 18:38:29 +0530

    Resolved Compliation Errors - And Merged the code properly

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -15,7 +15,6 @@ import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
-import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -30,142 +29,147 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/")
 public class AnswerController {
-  @Autowired private AnswerBusinessService answerBusinessService;
+    @Autowired
+    private AnswerBusinessService answerBusinessService;
 
-  @Autowired private UserBusinessService userAuthBusinessService;
+    @Autowired
+    private UserBusinessService userAuthBusinessService;
 
-  @Autowired private QuestionBusinessService questionBusinessService;
+    @Autowired
+    private QuestionBusinessService questionBusinessService;
 
-  @RequestMapping(
-      method = RequestMethod.POST,
-      path = "/question/{questionId}/answer/create",
-      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public ResponseEntity<AnswerResponse> createAnswer(
-      @RequestHeader("authorization") final String authorization,
-      @PathVariable("questionId") final String questionId,
-      final AnswerRequest answerRequest)
-      throws AuthorizationFailedException, InvalidQuestionException {
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path = "/question/{questionId}/answer/create",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerResponse> createAnswer(
+            @RequestHeader("authorization") final String authorization,
+            @PathVariable("questionId") final String questionId,
+            final AnswerRequest answerRequest)
+            throws AuthorizationFailedException, InvalidQuestionException {
 
 
-    final UserAuthenticationEntity userAuthEntity = userAuthBusinessService.getUser(authorization);
+        //  final UserAuthenticationEntity userAuthEntity = userAuthBusinessService.getUser(authorization);
 
-    final UserAuthenticationEntity userAuthEntity =
-        userAuthBusinessService.authorizeUser(authorization);
+        final UserAuthenticationEntity userAuthEntity =
+                userAuthBusinessService.authorizeUser(authorization);
 
-    QuestionEntity questionEntity = questionBusinessService.CheckValidQuestion(questionId);
+        QuestionEntity questionEntity = questionBusinessService.CheckValidQuestion(questionId);
 
-    UserEntity userEntity = userAuthEntity.getUser();
+        UserEntity userEntity = userAuthEntity.getUser();
 
-    AnswerEntity answerEntity = new AnswerEntity();
-    answerEntity.setUuid(UUID.randomUUID().toString());
-    answerEntity.setDate(ZonedDateTime.now());
-    answerEntity.setQuestion(questionEntity);
-    answerEntity.setAnswer(answerRequest.getAnswer());
-    answerEntity.setUser(userEntity);
+        AnswerEntity answerEntity = new AnswerEntity();
+        answerEntity.setUuid(UUID.randomUUID().toString());
+        answerEntity.setDate(ZonedDateTime.now());
+        answerEntity.setQuestion(questionEntity);
+        answerEntity.setAnswer(answerRequest.getAnswer());
+        answerEntity.setUser(userEntity);
 
-    AnswerEntity createdAnswer = answerBusinessService.createAnswer(answerEntity, userAuthEntity);
-    AnswerResponse answerResponse =
-        new AnswerResponse().id(createdAnswer.getUuid()).status("ANSWER CREATED");
+        AnswerEntity createdAnswer = answerBusinessService.createAnswer(answerEntity, userAuthEntity);
+        AnswerResponse answerResponse =
+                new AnswerResponse().id(createdAnswer.getUuid()).status("ANSWER CREATED");
 
-    return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.OK);
-  }
-
-  @RequestMapping(
-
-      method = RequestMethod.DELETE,
-      path = "/answer/delete/{answerId}",
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public ResponseEntity<AnswerDeleteResponse> deleteAnswer(
-      @RequestHeader("authorization") final String authorization,
-      @PathVariable("answerId") final String answerId)
-      throws AuthorizationFailedException, AnswerNotFoundException {
-    // Authorize User, if user registered and Logged in?
-    final UserAuthenticationEntity userAuthEntity =
-        userAuthBusinessService.authorizeUser(authorization);
-
-    // Get the Logged In User Details
-    final UserEntity currentUser = userAuthEntity.getUser();
-
-    // Check if the USer is Admin Role
-    final boolean isAdminUser = "Admin".equalsIgnoreCase(currentUser.getRole());
-
-    // Load the current Answer to be Deleted.
-    AnswerEntity answerEntity = answerBusinessService.getAnswerEntity(answerId);
-
-    // Lets Check if the user is the Owner of this Answer
-    final boolean isAnswerOwner = currentUser.getUuid().equals(answerEntity.getUser().getUuid());
-
-    // Only a Admin User or the Answer Owner Can delete the Answer.
-    if (isAdminUser || isAnswerOwner) {
-
-      // Lets Delete the Answer
-      AnswerDeleteResponse answerDeleteResponse = null;
-      AnswerEntity deletedAnswer = this.answerBusinessService.deleteAnswer(answerEntity);
-      if (deletedAnswer == null) {
-        answerDeleteResponse =
-            new AnswerDeleteResponse()
-                .id(answerEntity.getUuid())
-                .status("ANSWER COULD NOT BE DELETED");
-      } else {
-        answerDeleteResponse =
-            new AnswerDeleteResponse().id(answerEntity.getUuid()).status("ANSWER DELETED");
-      }
-
-      return new ResponseEntity<AnswerDeleteResponse>(answerDeleteResponse, HttpStatus.OK);
-    }
-    throw new AuthorizationFailedException(
-        "ATHR-003", "Only the answer owner or admin can delete the answer");
-=======
-      method = RequestMethod.PUT,
-      path = "/answer/edit/{answerId}",
-      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public ResponseEntity<AnswerEditResponse> editAnswer(
-      @RequestHeader("authorization") final String authorization,
-      @PathVariable("answerId") final String answerId,
-      final AnswerEditRequest answerEditRequest)
-      throws AuthorizationFailedException, AnswerNotFoundException {
-
-    UserAuthenticationEntity userAuthEntity = userAuthBusinessService.authorizeUser(authorization);
-
-    AnswerEntity answerEntity = answerBusinessService.getAnswerById(answerId);
-    AnswerEntity checkedAnswer =
-        answerBusinessService.verifyAnswerBelongsToUser(userAuthEntity, answerEntity);
-
-    checkedAnswer.setAnswer(answerEditRequest.getContent());
-    AnswerEntity updatedAnswer = answerBusinessService.updateAnswer(checkedAnswer);
-
-    AnswerEditResponse answerEditResponse =
-        new AnswerEditResponse().id(updatedAnswer.getUuid()).status("ANSWER EDITED");
-
-    return new ResponseEntity<AnswerEditResponse>(answerEditResponse, HttpStatus.OK);
-  }
-
-  @RequestMapping(
-      method = RequestMethod.GET,
-      path = "answer/all/{questionId}",
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswersToQuestion(
-      @RequestHeader("authorization") final String authorization,
-      @PathVariable("questionId") final String questionId)
-      throws AuthorizationFailedException, InvalidQuestionException {
-
-    UserAuthenticationEntity userAuthEntity = userAuthBusinessService.authorizeUser(authorization);
-    QuestionEntity questionEntity = questionBusinessService.CheckValidQuestion(questionId);
-
-    ArrayList<AnswerDetailsResponse> list = new ArrayList<>();
-    ArrayList<AnswerEntity> answers =
-        (ArrayList) answerBusinessService.getAllAnswersToQuestion(questionId, userAuthEntity);
-
-    for (AnswerEntity answer : answers) {
-      AnswerDetailsResponse detailsResponse = new AnswerDetailsResponse();
-      detailsResponse.setId(answer.getUuid());
-      detailsResponse.setAnswerContent(answer.getAnswer());
-      detailsResponse.setQuestionContent(questionEntity.getContent());
-      list.add(detailsResponse);
+        return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.OK);
     }
 
-    return new ResponseEntity<List<AnswerDetailsResponse>>(list, HttpStatus.OK);
-  }
+    @RequestMapping(
+
+            method = RequestMethod.DELETE,
+            path = "/answer/delete/{answerId}",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerDeleteResponse> deleteAnswer(
+            @RequestHeader("authorization") final String authorization,
+            @PathVariable("answerId") final String answerId)
+            throws AuthorizationFailedException, AnswerNotFoundException {
+        // Authorize User, if user registered and Logged in?
+        final UserAuthenticationEntity userAuthEntity =
+                userAuthBusinessService.authorizeUser(authorization);
+
+        // Get the Logged In User Details
+        final UserEntity currentUser = userAuthEntity.getUser();
+
+        // Check if the USer is Admin Role
+        final boolean isAdminUser = "Admin".equalsIgnoreCase(currentUser.getRole());
+
+        // Load the current Answer to be Deleted.
+        AnswerEntity answerEntity = answerBusinessService.getAnswerEntity(answerId);
+
+        // Lets Check if the user is the Owner of this Answer
+        final boolean isAnswerOwner = currentUser.getUuid().equals(answerEntity.getUser().getUuid());
+
+        // Only a Admin User or the Answer Owner Can delete the Answer.
+        if (isAdminUser || isAnswerOwner) {
+
+            // Lets Delete the Answer
+            AnswerDeleteResponse answerDeleteResponse = null;
+            AnswerEntity deletedAnswer = this.answerBusinessService.deleteAnswer(answerEntity);
+            if (deletedAnswer == null) {
+                answerDeleteResponse =
+                        new AnswerDeleteResponse()
+                                .id(answerEntity.getUuid())
+                                .status("ANSWER COULD NOT BE DELETED");
+            } else {
+                answerDeleteResponse =
+                        new AnswerDeleteResponse().id(answerEntity.getUuid()).status("ANSWER DELETED");
+            }
+
+            return new ResponseEntity<AnswerDeleteResponse>(answerDeleteResponse, HttpStatus.OK);
+        }
+        throw new AuthorizationFailedException(
+                "ATHR-003", "Only the answer owner or admin can delete the answer");
+
+    }
+
+    @RequestMapping( method = RequestMethod.PUT,
+            path = "/answer/edit/{answerId}",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerEditResponse> editAnswer(
+            @RequestHeader("authorization") final String authorization,
+            @PathVariable("answerId") final String answerId,
+            final AnswerEditRequest answerEditRequest)
+            throws AuthorizationFailedException, AnswerNotFoundException {
+
+        UserAuthenticationEntity userAuthEntity = userAuthBusinessService.authorizeUser(authorization);
+
+        AnswerEntity answerEntity = answerBusinessService.getAnswerById(answerId);
+        AnswerEntity checkedAnswer =
+                answerBusinessService.verifyAnswerBelongsToUser(userAuthEntity, answerEntity);
+
+        checkedAnswer.setAnswer(answerEditRequest.getContent());
+        AnswerEntity updatedAnswer = answerBusinessService.updateAnswer(checkedAnswer);
+
+        AnswerEditResponse answerEditResponse =
+                new AnswerEditResponse().id(updatedAnswer.getUuid()).status("ANSWER EDITED");
+
+        return new ResponseEntity<AnswerEditResponse>(answerEditResponse, HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "answer/all/{questionId}",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswersToQuestion(
+            @RequestHeader("authorization") final String authorization,
+            @PathVariable("questionId") final String questionId)
+            throws AuthorizationFailedException, InvalidQuestionException {
+
+        UserAuthenticationEntity userAuthEntity = userAuthBusinessService.authorizeUser(authorization);
+        QuestionEntity questionEntity = questionBusinessService.CheckValidQuestion(questionId);
+
+        ArrayList<AnswerDetailsResponse> list = new ArrayList<>();
+        ArrayList<AnswerEntity> answers =
+                (ArrayList) answerBusinessService.getAllAnswersToQuestion(questionId, userAuthEntity);
+
+        for (AnswerEntity answer : answers) {
+            AnswerDetailsResponse detailsResponse = new AnswerDetailsResponse();
+            detailsResponse.setId(answer.getUuid());
+            detailsResponse.setAnswerContent(answer.getAnswer());
+            detailsResponse.setQuestionContent(questionEntity.getContent());
+            list.add(detailsResponse);
+        }
+
+        return new ResponseEntity<List<AnswerDetailsResponse>>(list, HttpStatus.OK);
+    }
 }
--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
@@ -16,66 +16,71 @@ import java.util.List;
 @Service
 public class AnswerBusinessService {
 
-  @Autowired private AnswerDao answerDao;
+    @Autowired
+    private AnswerDao answerDao;
 
-  @Transactional(propagation = Propagation.REQUIRED)
-  public AnswerEntity createAnswer(
-      AnswerEntity answerEntity, UserAuthenticationEntity userAuthEntity)
-      throws AuthorizationFailedException {
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity createAnswer(
+            AnswerEntity answerEntity, UserAuthenticationEntity userAuthEntity)
+            throws AuthorizationFailedException {
 
-    if (userAuthEntity.getLogoutAt() != null) {
-      throw new AuthorizationFailedException(
-          "ATHR-002", "User is signed out.Sign in first to post an answer");
-    }
-    return answerDao.createAnswer(answerEntity);
-  }
-
-  @Transactional(propagation = Propagation.REQUIRED)
-  public AnswerEntity getAnswerById(String uuid) throws AnswerNotFoundException {
-    AnswerEntity answerEntity = answerDao.getAnswerById(uuid);
-    if (answerEntity == null) {
-      throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
-    }
-    return answerEntity;
-  }
-
-  @Transactional(propagation = Propagation.REQUIRED)
-  public AnswerEntity verifyAnswerBelongsToUser(
-      UserAuthenticationEntity userAuthEntity, AnswerEntity answerEntity)
-      throws AuthorizationFailedException {
-
-    UserEntity userEntity = userAuthEntity.getUser();
-
-    if (userAuthEntity.getLogoutAt() != null) {
-      throw new AuthorizationFailedException(
-          "ATHR-002", "User is signed out.Sign in first to edit an answer");
-    }
-    String auuid = answerEntity.getUuid();
-    String uuuid = userEntity.getUuid();
-    AnswerEntity checkedAnswer = answerDao.verifyAnswerBelongsToUser(auuid, uuuid);
-    if (checkedAnswer == null) {
-      throw new AuthorizationFailedException(
-          "ATHR-003", "Only the answer owner can edit or delete the answer");
-    }
-    return checkedAnswer;
-  }
-
-  @Transactional(propagation = Propagation.REQUIRED)
-  public AnswerEntity updateAnswer(AnswerEntity answerEntity) {
-    AnswerEntity updateAnswer = answerDao.updateAnswer(answerEntity);
-    return updateAnswer;
-  }
-
-  public List<AnswerEntity> getAllAnswersToQuestion(
-      String questionId, UserAuthenticationEntity userAuthEntity)
-      throws AuthorizationFailedException {
-    if (userAuthEntity.getLogoutAt() != null) {
-      throw new AuthorizationFailedException(
-          "ATHR-002", "User is signed out.Sign in first to get the answers");
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to post an answer");
+        }
+        return answerDao.createAnswer(answerEntity);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity getAnswerById(String uuid) throws AnswerNotFoundException {
+        AnswerEntity answerEntity = answerDao.getAnswerById(uuid);
+        if (answerEntity == null) {
+            throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+        }
+        return answerEntity;
+    }
 
-    public AnswerEntity getAnswerEntity(String answer_uuId)  throws AnswerNotFoundException {
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity verifyAnswerBelongsToUser(
+            UserAuthenticationEntity userAuthEntity, AnswerEntity answerEntity)
+            throws AuthorizationFailedException {
+
+        UserEntity userEntity = userAuthEntity.getUser();
+
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to edit an answer");
+        }
+        String auuid = answerEntity.getUuid();
+        String uuuid = userEntity.getUuid();
+        AnswerEntity checkedAnswer = answerDao.verifyAnswerBelongsToUser(auuid, uuuid);
+        if (checkedAnswer == null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-003", "Only the answer owner can edit or delete the answer");
+        }
+        return checkedAnswer;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity updateAnswer(AnswerEntity answerEntity) {
+        AnswerEntity updateAnswer = answerDao.updateAnswer(answerEntity);
+        return updateAnswer;
+    }
+
+    public List<AnswerEntity> getAllAnswersToQuestion(
+            String questionId, UserAuthenticationEntity userAuthEntity)
+            throws AuthorizationFailedException {
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to get the answers");
+        }
+
+        return answerDao.getAllAnswersToQuestion(questionId);
+    }
+
+
+    public AnswerEntity getAnswerEntity(String answer_uuId)
+            throws AnswerNotFoundException {
         return answerDao.loadAnswer(answer_uuId);
     }
 
@@ -83,7 +88,4 @@ public class AnswerBusinessService {
     public AnswerEntity deleteAnswer(AnswerEntity answerEntity) {
         return answerDao.deleteAnswer(answerEntity);
     }
-
-    return answerDao.getAllAnswersToQuestion(questionId);
-  }
 }
